ce4667faa271b1f00b4d235c2af47396290c482a
Mohamed_Ali_Abdelmalek <abdelmalek.medali@gmail.com>
2025-06-28 01:35:12 +0100

    name fixing

--- src/App.vue
+++ src/App.vue
@@ -557,7 +557,7 @@ watch(isDark, (val) => {
 
 .stations-grid {
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   gap: 1.5rem;
   padding-bottom: 2rem;
 }
